a42eccc4582eae66685fa69a8cc3d83181d17305
ANSHUMAN BISWAS <ANSHUMAN BISWAS@DESKTOP-DT60913>
2022-02-14 16:30:47 +0530

    first commit

---
+++ firstprogram/src/firstprogram/typecasting.java
@@ -0,0 +1,11 @@
+package firstprogram;
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		float f = 10.5f;
+		int a = (int) f;
+		System.out.println(f);
+		System.out.println(a);
+	}
+}
