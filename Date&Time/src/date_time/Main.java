64212dd448c81bdea84490a05a831cea1c2a97c5
ANSHUMAN BISWAS <ANSHUMAN BISWAS@DESKTOP-DT60913>
2022-02-21 11:35:51 +0530

    displaying current date and default date

---
+++ Date&Time/src/date_time/Main.java
@@ -0,0 +1,14 @@
+package date_time;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Date d1 = new Date();
+		System.out.println("Current Date is : "+d1);
+		Date d2 = new Date(1);
+		System.out.println("Date Representation is : "+d2);
+	}
+
+}
