f99d3fbb1b357d2d830d03a3f3dccdac6efb8e1a
ANSHUMAN BISWAS <ANSHUMAN BISWAS@DESKTOP-DT60913>
2022-03-11 18:52:27 +0530

    Case Study on Online Shopping Application Called As GoShoppingApp

--- GoShoppingApp/src/com/tns/framework/ShopFactory.java
+++
@@ -1,8 +0,0 @@
-package com.tns.framework;
-
-public abstract class ShopFactory {
-	public abstract PrimeAcc getNewPrimeAccount(int AccNo, String accNm, float charges, boolean isPrime);
-	public abstract NormalAcc getNewNormalAccount(int AccNo, String accNm,float charges, float deliveryCharge);
-	
-
-}
