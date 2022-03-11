fb3d0d7e38b48ebd09011427b91d7abf2b039347
ANSHUMAN BISWAS <ANSHUMAN BISWAS@DESKTOP-DT60913>
2022-03-11 20:48:00 +0530

    abc

--- firstprogram/src/firstprogram/Test1.java
+++ firstprogram/src/firstprogram/Test1.java
@@ -3,7 +3,11 @@ package firstprogram;
 public class Test1 {
 	public static void main (String[] args)
 	{
-		for (int i=0;i<1;System.out.println("HI"))
-		System.out.println("GREAT");
+		//for (int i=0;i<1;System.out.println("HI"))
+		System.out.println(fun());
+	}
+	static int fun()
+	{
+		return 20;
 	}
 }
